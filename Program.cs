7e487ae19430ea5030dd941e0e02f1f535969b54
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-01-25 14:48:20 +0100

    UI and main menu done.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to school!\n\nMenu:");
+            Console.WriteLine("1. List all students");
+            Console.WriteLine("2. List all students in a certain class");
+            Console.WriteLine("3. Add new staff");
+            Console.WriteLine("4. List staff");
+            Console.WriteLine("5. List grades set in the last month");
+            Console.WriteLine("6. List grade statistics from all courses");
+            Console.WriteLine("7. Add new student");
+            Console.WriteLine("8. Exit");
+
+            int menuInput;
+            bool parseSuccess;
+            do
+            {
+                Console.Write("\nEnter a valid number: ");
+                parseSuccess = int.TryParse(Console.ReadLine(), out menuInput);
+            } while (!parseSuccess || menuInput < 1 || menuInput > 8);
         }
     }
 }
