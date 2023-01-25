65d80c92971afdf3d4c3fe88c06fb3a56a85f5a1
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-01-25 14:47:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UsingEntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
